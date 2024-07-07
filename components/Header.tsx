17ff4ea29281c80b7bac042786cc0c58e6de26b4
Ghazi Hussain <alinaqi7214125@gmail.com>
2024-07-08 01:05:48 +0500

    change contact to aboutus

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => (
       <ul className="flex space-x-6">
         <li><a href="#home" className="hover:underline">Home</a></li>
         <li><a href="#menu" className="hover:underline">Menu</a></li>
-        <li><a href="#contact" className="hover:underline">Contact</a></li>
+        <li><a href="#contact" className="hover:underline">Aboutus</a></li>
       </ul>
     </nav>
     <CartIcon />
